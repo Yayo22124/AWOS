df567f0c3b8b2c5181c62d6981bcf63425657d2e
Haziel Ortiz <ya-yo22124@outlook.com>
2023-09-06 08:02:59 -0600

    Initial Commit

---
+++ RepasoJS-220087/js/01.mjs
@@ -0,0 +1,12 @@
+// --------------------------------------------------------------------------------
+//                          Repaso de JavaScript
+// Practica 01: Declaración de Variables.
+// Realizado por: Eli Haziel Ortiz Ramirez 220087 4B DSM
+// 
+// --------------------------------------------------------------------------------
+
+// Formas de Declarar Variables
+
+// VAR
+    
+// 
