92f9d235b29c7e910a44676b2b1f4bcc76cc8de3
Haziel Ortiz <ya-yo22124@outlook.com>
2023-09-15 06:32:14 -0600

    Añadiendo ejercicios de Arreglos

--- RepasoJS-220087/js/Practica07.mjs
+++ RepasoJS-220087/js/Practica07.mjs
@@ -9,6 +9,7 @@
 const bg3 = 'radial-gradient(circle, rgba(95,251,63,1) 0%, rgba(70,252,174,0.907527977011117) 100%)';
 
 const style4 = `background: ${bg3}; color: white; border-radius: 12px; padding: 4px; font-size: 1.0rem; font-weight: bold;`;
+const style5 = `background: gray; color: white; border-radius: 12px; padding: 4px 3px;margin-left: 20px; font-size: 1.0rem; font-weight: bold;`;
 const logStyle = 'background: #222; font-weight: bold;'
 
 
@@ -51,11 +52,72 @@ console.log(`El arregloMixto tiene: ${arregloMixto.length}, elementos.`);
 
 // Agregar elementos a un array (al final)
 console.log("Para agregar elementos a un array al final del mismo se usa el método PUSH  ");
-meses.push('Diciembre');
+meses.push('Gato');
 console.table(meses);
 
 // Agregar elementos a un array (al inicio)
-console.log("Para agregar elementos a un array al inicio del mismo se usa el método PUSH  ");
-meses.unshift('Enero')
+console.log("Para agregar elementos a un array al inicio del mismo se usa el método UNSHIFT  ");
+meses.unshift('Perro')
 console.table(meses);
 
+// Eliminar el elemento en la última posición: POP
+console.log(`Para eliminar el elemento en la última posición se utiliza el método: POP`);
+console.log(`Eliminando el elemento en la última posición: ${meses[meses.length - 1]}`);
+meses.pop();
+meses.push("Diciembre")
+console.log("El arreglo actual es: ");
+console.table(meses);
+
+// Eliminar el elemento en la primera posición: SHIFT
+console.log(`Para eliminar el elemento en la primera posición: ${meses[0]}`);
+meses.shift();
+meses.unshift("Enero");
+
+
+// Dividir el arreglo orifinal, sacar los primeros 3 elementos
+console.log('Dividir el arreglo, sacar los primeros 3 elementos. ');
+// meses.splice(3) Si no mando 2 parametros, elimina los restantes.
+meses.splice(7, 1);
+console.table(meses);
+
+// Metodos para la manipulación de arreglos inmutables
+console.log("%c4.- Métodos para la manipulación de arreglos inmutables", style4);
+
+// Destructuración de arreglos
+// console.log("%c5.- Destructuración de Arreglos", style4);
+// let [signo0, signo1, signo2, signo3, signo4, signo5, signo6, signo7] = signosZodiacales;
+// console.log(`El signo zodiacal en la posicion [0]: ${signo0}`);
+// console.log(`El signo zodiacal en la posicion [7]: ${signo7}`);
+
+// let [signo_0,,,,,,signo_7] = signosZodiacales;
+// console.log(`El signo zodiacal en la posicion [0]: ${signo_0}`);
+// console.log(`El signo zodiacal en la posicion [7]: ${signo_7}`);
+
+// // Destructuración de arreglos
+// console.log("%c6.- Métodos de Arreglos que crean un nuevo arreglo, para no modificar el original", style4);
+// // ---- Filter ----
+// console.log("%c6.1.- Filter", style5);
+// console.log(`El arreglo original es: `);
+// console.log(signosZodiacales);
+// console.log(`Filtrando Datos........`);
+// let signosFiltrados = signosZodiacales.Filter((signosZodiacales) => signosZodiacales.length > 6);
+// console.log("Filtrado terminado");
+// console.log("El arreglo resultante es: ");
+// console.log(signosFiltrados);
+
+// ---- Map ----
+console.log("%c6.1.- Map", style5);
+const numeros = [1,2,3,4,5];
+const dobles = numeros.map( numero => numero*2);
+console.log("El arreglo original es: ");
+console.log(numeros);
+console.log("El arreglo del doble de los elementos del original es: ");
+console.log(dobles);
+
+// ---- Reduce ----
+console.log("%c6.1.- Reduce", style5);
+const numeros2 = [1,30,25,12,-5,17.5]
+const sumaTotal = numeros2.reduce((totalParcial, elemento) => totalParcial + elemento)
+console.log("El arreglo de número original es: ");
+console.log(numeros2);
+console.log(`La suma de los elementos es: ${sumaTotal}`);
